efb98fc4c837a67b453c596d6fce03b6f7faf2d0
Amardeep840690 <amar840690@gmail.com>
2025-12-03 22:18:39 +0530

    add new file swap

---
+++ swap.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class swap {
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number a = ");
+        int a=sc.nextInt();
+        System.out.print("enter a number b = ");
+        int b=sc.nextInt();
+        System.out.println("before swaping a is "+a+" and b is "+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after swaping a is "+a+" and b is "+b);
+    }
+}
